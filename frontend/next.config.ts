60aca7156b0af76f8eab1a730e21db7f53c498f3
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-04-08 14:09:52 +0700

    tailwind config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  // webpack: (config, { isServer }) => {
-  //   if (!isServer) {
-  //     config.resolve.fallback = {
-  //       fs: false, // Prevents 'fs' from causing issues in the browser
-  //     };
-  //   }
-  //   return config;
-  // },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
