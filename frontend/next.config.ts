f34670eae91f673516cad08fdd58331ec7c16242
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-04-25 09:51:23 +0700

    config url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: '127.0.0.1',
+            port: '1337',
+            pathname: '/uploads/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
