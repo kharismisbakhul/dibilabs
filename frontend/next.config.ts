05de43a9dfdfc05c1df441fed4fca6c1fd024fd0
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-04-29 10:22:42 +0700

    eslint next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: '127.0.0.1',
-            port: '1337',
-            pathname: '/uploads/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
       },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 Add this
+  },
 };
 
 export default nextConfig;
