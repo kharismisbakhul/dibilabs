f07899a8536fa6a2805321c0c73c9df4174276e3
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-05-16 13:09:12 +0700

    add animation config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx}","./src/components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'scroll-ltr': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        'scroll-rtl': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        'zoom-pulse': {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.05)' },
+        },
+        'bounce-smooth': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        'marqueeLeft': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        'marqueeRight': {
+          '0%': { transform: 'translateX(-50%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation: {
+        'scroll-ltr': 'scroll-ltr 30s linear infinite',
+        'scroll-rtl': 'scroll-rtl 30s linear infinite',
+        'pulse-slow': 'zoom-pulse 3s ease-in-out infinite',
+        'bounce-smooth': 'bounce-smooth 2s ease-in-out infinite',
+        'marqueeLeft': 'marqueeLeft 30s linear infinite',
+        'marqueeRight': 'marqueeRight 30s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
