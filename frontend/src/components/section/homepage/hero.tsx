099fc8653dc414fbde649263d8b2f5a55fb0651a
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-05-18 11:13:10 +0700

    add flex on hero homepage

--- frontend/src/components/section/homepage/hero.tsx
+++ frontend/src/components/section/homepage/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <>
       <div className="bg-orange-500 text-white">
-        <div className="container mx-auto px-4">
+        <div className="">
           {/* Hero Section */}
           <section className="px-6 md:px-16 py-12 md:py-20 relative overflow-hidden">
             <div className="grid md:grid-cols-2 gap-10 items-center">
@@ -26,20 +26,22 @@ export default function Hero() {
                   deliver{" "}
                   <span className="font-bold">beyond expectations!</span>
                 </p>
-                <div className="flex items-center gap-4 mb-6">
-                  <p className="text-3xl font-light m-0">
+                {/* Button & Text Row */}
+                <div className="w-full flex flex-col sm:flex-row items-center sm:justify-between gap-4">
+                  <p className="text-xl lg:text-3xl font-light m-0 text-center sm:text-left flex-1 whitespace-nowrap">
                     Navigate the Digital Ecosystem with Dibilabs.id!
                   </p>
-                  <a href="#footer">
-                    <button className="bg-white text-black px-6 py-3 rounded-full flex items-center gap-2 font-semibold hover:bg-black hover:text-white">
+                  <a href="#footer" className="shrink-0">
+                    <button className="bg-white text-black px-6 py-3 rounded-full flex items-center gap-2 font-semibold hover:bg-black hover:text-white whitespace-nowrap">
                       Grow Together <ArrowRight className="w-5 h-5" />
                     </button>
                   </a>
                 </div>
+
               </div>
 
               {/* SVG Character - Right */}
-              <div className="relative w-full max-w-md mx-auto aspect-[4/5] order-1 md:order-2 animate-pulse-slow">
+              <div className="relative w-full max-w-md mx-auto aspect-[4/5] order-1 animate-pulse-slow">
                 <Image
                   src="/assets/homepage/beaker.png"
                   alt="Beaker Mascot"
