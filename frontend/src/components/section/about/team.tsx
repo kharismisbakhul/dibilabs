281b2ca77153e0abcdadf12b9808fc7636f3d89b
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-05-21 11:55:39 +0700

    fix grid

--- frontend/src/components/section/about/team.tsx
+++ frontend/src/components/section/about/team.tsx
@@ -20,7 +20,7 @@ export default function Team({ data }: Props) {
       </div>
 
       {/* Grid of images */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      <div className="grid grid-cols-3 md:grid-cols-3">
         {data.map((item, i) => (
           <div
             key={i}
