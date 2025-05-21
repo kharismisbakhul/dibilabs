f011905c33be3f36f801d10ceca9e2281e8ef098
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-05-21 07:59:26 +0700

    contact responsive

--- frontend/src/components/section/contact/heroContact.tsx
+++ frontend/src/components/section/contact/heroContact.tsx
@@ -10,7 +10,7 @@ export default function HeroContact() {
             <div className="grid md:grid-cols-2 gap-10 items-center p-10">
               {/* Text Content - Left */}
               <div className="order-2 md:order-1">
-                <h1 className="text-[200px] font-bold mb-4">
+                <h1 className="text-7xl md:text-[200px] font-bold mb-4">
                   Contact <br />
                   us! <br />
                 </h1>
@@ -18,7 +18,7 @@ export default function HeroContact() {
 
               {/* SVG Character - Right */}
               {/* Top Right Bubble Decoration */}
-              <div className="absolute right-0 top-0 z-0">
+              <div className="absolute w-[130px] md:w-[400px] right-0 top-0 z-0">
                 <Image
                   src="/assets/contact/right_bubble.svg"
                   alt="Bubbles Decoration"
@@ -28,7 +28,7 @@ export default function HeroContact() {
                 />
               </div>
 
-              <div className="absolute right-[600px] top-[300px] animate-float">
+              <div className="absolute right-0 right-[150px] top-[120px] md:right-[600px] md:top-[300px] w-[120px] md:w-[350px] animate-float">
                 <Image
                   src="/assets/contact/eyes.svg"
                   alt="Bubbles Decoration"
@@ -44,7 +44,7 @@ export default function HeroContact() {
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-[-3deg] w-full">
             <div className="flex animate-marquee w-max">
               {Array(2).fill(0).map((_, i) => (
-                <div key={i} className="flex text-white text-4xl font-bold bg-[#231F20] px-4 py-6">
+                <div key={i} className="flex text-white text-2xl md:text-4xl font-bold bg-[#231F20] px-4 py-6">
                   {Array(30).fill(" ✦ DIBILABS ").map((text, index) => (
                     <span key={index} className="px-1 whitespace-nowrap">{text}</span>
                   ))}
@@ -58,7 +58,7 @@ export default function HeroContact() {
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-[-3deg] w-full">
             <div className="flex animate-marquee w-max">
               {Array(2).fill(0).map((_, i) => (
-                <div key={i} className="flex text-white text-4xl font-bold bg-[#065097] px-4 py-6">
+                <div key={i} className="flex text-white text-2xl md:text-4xl font-bold bg-[#065097] px-4 py-6">
                   {Array(30).fill(" ✦ DIBILABS ").map((text, index) => (
                     <span key={index} className="px-1 whitespace-nowrap">{text}</span>
                   ))}
