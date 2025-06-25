6c98bac1212f0d7f8f1ff6b8d740f6832d4770d9
Misbakhul Kharis <kharismisbakhul@gmail.com>
2025-06-25 01:04:10 +0000

    add google analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "@/components/core/googleAnalytics";
 // import ResponsiveWrapper from "@/components/core/responsiveWrapper";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black`}
       >
+        <GoogleAnalytics />
         {/* <ResponsiveWrapper> ✅ Wrap children here */}
           {children}
         {/* </ResponsiveWrapper> */}
---
+++ frontend/src/components/core/googleAnalytics.tsx
@@ -0,0 +1,22 @@
+'use client'
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-9D9F1QN80N"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9D9F1QN80N');
+        `}
+      </Script>
+    </>
+  )
+}
